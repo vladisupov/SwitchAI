f858bdc58a1a877db0e44d8bbf3399b8e2fd0905
vladislav <vlad.isupov.09@mail.ru>
2026-05-09 10:53:36 +0300

    Полностью рабочая страница neuro (пользователь выбирает модель, пишет промпт и получает ответ от выбранной нейросети)

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import requests
 from flask import Flask, render_template, redirect, request, make_response, jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from data import db_session
 from data.users import User
 from forms.login_form import LoginForm
 from forms.register_form import RegisterForm
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Берем ключ для приложения и api key из .env(так безопаснее)
+OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
+OPENROUTER_URL = "https://openrouter.ai/api/v1/responses"
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -17,23 +26,21 @@ def main():
     db_session.global_init("db/switch.db")
     app.run(port=8080)
 
+#------------------------------------------------Главная страница сайта-------------------------------------------
+
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
     users = db_sess.query(User).all()
     names = {name.id: (name.surname, name.name) for name in users}
     return render_template("index.html", names=names, title='Главная')
-
+                                #Загрузка пользователя
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
+#-------------------------------------------Обработка страницы и формы входа. Вход---------------------------------------
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -47,13 +54,14 @@ def login():
         return render_template('login.html', message="Wrong login or password", form=form)
     return render_template('login.html', title='Authorization', form=form)
 
+#------------------------------------------------Выход из аккаунта------------------------------------------
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect("/")
 
-
+#-----------------------------------------Обработка страницы регистрации. Регистрация---------------------------------------
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     form = RegisterForm()
@@ -76,10 +84,50 @@ def reqister():
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
+#--------------------------------------------Страница с нейросетями---------------------------------------
 @app.route('/neuro')
 def neuro():
     db_sess = db_session.create_session()
     return render_template('neuro.html', title='Нейросети')
+
+
+#-------------------------------------------Работа с API-----------------------------------------------
+
+#Здесь будет обрабатываться запрос с javascript,
+# отправление запроса и получение ответа без обновления страницы
+@app.route('/neuro_request', methods=['POST'])
+def neuro_request():
+    data = request.json
+    user_prompt = data.get('prompt', '')
+    model = data.get('model', '')
+
+    if not(user_prompt):
+        return jsonify({'error': 'No prompt provided'}), 400
+    if not(model):
+        return jsonify({'error': 'No model provided'}), 400
+
+    headers = {
+        "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+        "X-OpenRouter-Experimental-Metadata": "enabled"
+    }
+    #Тело запроса
+    json = {
+        "input": f"{user_prompt}",
+        "model": f"{model}",
+    }
+
+    #Запрос и получение ответа
+    try:
+        response = requests.post(OPENROUTER_URL, headers=headers, json=json)
+        response.raise_for_status()
+        result = response.json()
+
+        ai_message = result['output'][1]['content'][0]['text']
+        return jsonify({'message': ai_message})
+
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': str(e)}), 500
 
 
 
